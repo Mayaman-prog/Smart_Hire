70fe5b9721015e8b417bf2bcb3eab37f444e6bd1
Nischal Prajapati <np03cy4s250002@heraldcollege.edu.np>
2026-04-05 23:05:15 +0545

    feat: implement MySQL database schema with users, companies, jobs, applications tables

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,50 @@
-ÿþ
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../../database/connection');
+
+// Define User model schema
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true }
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  firstName: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('job_seeker', 'employer', 'admin'),
+    defaultValue: 'job_seeker'
+  },
+  phone: {
+    type: DataTypes.STRING(20),
+    allowNull: true
+  },
+  avatar: {
+    type: DataTypes.STRING(500),
+    allowNull: true
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  timestamps: true,
+  tableName: 'users'
+});
+
+module.exports = User;

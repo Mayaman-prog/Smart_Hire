91de94e9d6a789fbfa7cfe4dae69fc31587fe2fd
Nischal Prajapati <np03cy4s250002@heraldcollege.edu.np>
2026-04-06 00:24:30 +0545

    update client dependencies and fix AuthContext path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
